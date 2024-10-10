be81aab45c539d9be3d0f12a20761dbaaad22cab
Mariana Alvarez de Carvalho <marialvarez@Marianas-MacBook-Pro.local>
2024-10-10 20:26:21 -0300

    Create project and add .gitignore file

---
+++ MovieCollection/MovieCollection/MovieCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionApp.swift
+//  MovieCollection
+//
+//  Created by Mariana Alvarez de Carvalho on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
